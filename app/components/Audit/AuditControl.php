4f63a48d1e2ef13010c15e6fcefe6b7e3d825b92
looky <looky.msc@gmail.com>
2013-07-02 21:56:26 +0200

    Audit detail transakce

--- app/components/Audit/AuditControl.php
+++ app/components/Audit/AuditControl.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Nette,
 	Framework,
-	Grido;
+	Grido,
+	Grido\Components\Filters\Filter;
 
 
 
@@ -12,10 +13,7 @@ class AuditControl extends Framework\Application\UI\BaseControl
 {
 	/** @var \Model\API @inject */
 	public $api;
-	
-	/** @var \Model\Transaction\ITransactionFactory @inject */
-	public $transactionFactory;
-	
+		
 	
 	
 	public function renderBazaar()
@@ -30,13 +28,16 @@ class AuditControl extends Framework\Application\UI\BaseControl
 	{
 		$self = $this;
 		
-		$transactions = array();
-		foreach ($this->api->query(array('id' => 'transactions'))->txs as $tData) {
-			$tData->node = json_encode($tData->node);
-			$tmp = $this->transactionFactory->create();
-			$tmp->data = $tData;
-			$transactions[] = $tmp->toArray();
-		}
+		$transactions = array_map(function ($item) {
+			$item->node = json_encode($item->node);
+			return (array)$item;
+		}, $this->api->query(array('id' => 'transactions'))->txs);
+		
+		$tmp = array_values(array_unique(array_map(function ($item) {
+			return $item['type'];
+		}, $transactions)));
+		array_unshift($tmp, '');
+		$types = array_combine($tmp, $tmp);
 		
 		$grido = new Grido\Grid($this, $name);
 		$grido->setModel($transactions)
@@ -44,19 +45,23 @@ class AuditControl extends Framework\Application\UI\BaseControl
 				->setPerPageList(array(100, 200, 500, 1000))
 				->setTranslator($this->translator)
 				->setPrimaryKey('txId')
-				->setDefaultSort(array('txId' => 'asc'));
+				->setDefaultSort(array('txId' => 'asc'))
+				->setFilterRenderType(Filter::RENDER_OUTER);
 		
 		$grido->addColumn('txId', 'ID')
 				->setSortable();
 		
 		$grido->addColumn('userId', 'UID')
-				->setSortable();
+				->setSortable()
+				->setFilter(Filter::TYPE_NUMBER);
 		
 		$grido->addColumn('name', 'Uživatel')
-				->setSortable();
+				->setSortable()
+				->setFilter();
 				
 		$grido->addColumn('type', 'Typ')
-				->setSortable();
+				->setSortable()
+				->setFilter(Filter::TYPE_SELECT, $types);
 
 		$grido->addColumn('timestamp', 'Čas')
 				->setSortable()
@@ -66,8 +71,31 @@ class AuditControl extends Framework\Application\UI\BaseControl
 		
 		$grido->addColumn('node', 'Data')
 				->setSortable()
-				->setTruncate(100);
+				->setTruncate(100)
+				->setFilter();
+		
+		$grido->addAction('show', 'Podrobnosti')
+				->setIcon('list')
+				->setCustomHref(function ($item) use ($self) {
+					return $self->getPresenter()->lazyLink('showViewTransaction', array('id' => $item['txId']));
+				});
 		
 		return $grido;
+	}
+	
+	
+	
+	public function renderShowTransaction()
+	{
+		$template = $this->template;
+		$template->setFile(__DIR__ . '/transaction.latte');
+		
+		$id = $this->getPresenter()->getParameter('id');
+		$transactions = array_filter($this->api->query(array('id' => 'transactions'))->txs, function ($item) use ($id) {
+			return $item->txId == $id;
+		});
+		$template->transaction = array_pop($transactions);
+		
+		$template->render();
 	}
 }
